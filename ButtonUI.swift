8781b5429fd3d0a7a6c467065d2dc8146b8c0027
Takahiro <81788027+TakahirosGithub@users.noreply.github.com>
2024-06-23 18:31:18 +0900

    Add files via upload

---
+++ ButtonUI.swift
@@ -0,0 +1,32 @@
+//
+//  File.swift
+//  Hello.Swift
+//
+//  Created by Takahiro on 2024/06/23.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ButtonUI: View {
+    @State private var btnTapped = 0
+    var body: some View {
+        Button(action: {
+            btnTapped += 1
+            print("タップ")
+        }){
+            Image("Image")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150 , height: 150)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                .shadow(radius: 10)
+            
+        }
+    }
+}
+
+#Preview {
+    ButtonUI()
+        .modelContainer(for: Item.self, inMemory: true)
+}
